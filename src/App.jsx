1e9874a77491240bc821acbfb1d76f4211ce0429
Asadbek-pro567 <85244820+Asadbek-pro567@users.noreply.github.com>
2023-11-21 13:07:36 +0500

    asda

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Router } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.scss'
+
+function Home() {
+  return (
+    <div>
+        <h1>hi</h1>
+    </div>
+  )
+}
+
+export default Home
